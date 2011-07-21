43bf84e0bb6c38a02ca5dd91e66dde2335d60f96
hcnh174 <nelsonhayes4@gmail.com>
2011-07-21 16:53:51 +0900

    added reporting functions for nextgen data

--- src/main/r/nextgen.r
+++ src/main/r/nextgen.r
@@ -587,10 +587,20 @@ estimateReadCount <- function(mb)
 #estimateReadCount(112.5)
 #reportAminoAcidChange(config,'PXB0218-0007','NS3aa156',156)
 
+#####################################################################################3
 
+getCodonCountSubset <- function(config, subject, region, filetype, start, end=start, cutoff=0)
+{
+	filename <- concat(config@counts.dir,subject,'.',filetype,'.txt')
+	data <- loadDataFrame(filename)
+	data.subset <- data[which(data$region==region & data$aanum>=start &data$aanum<=end & data$count>=cutoff),]
+	data.subset$replicate <- factor(data.subset$replicate)
+	data.subset$aanum <- factor(data.subset$aanum)
+	return(data.subset)
+}
+#getCodonCountSubset(config,'PXB0218-0007','NS3aa156','codons',156)
 
-
-reportAminoAcidChange <- function(config, subject, region, log=TRUE, updown=5)
+reportAminoAcidChange <- function(config, subject, region, log=FALSE, updown=5)
 {
 	aanum <- config@regions[region,'focus']
 	if (!is.integer(aanum))
@@ -598,12 +608,9 @@ reportAminoAcidChange <- function(config, subject, region, log=TRUE, updown=5)
 	aanum <- as.integer(aanum)	
 	start <- aanum - updown
 	end <- aanum + updown
-	filename <- concat(config@counts.dir,subject,'.aa.txt')
-	data <- loadDataFrame(filename)
-	rgn <- region
-	data.subset <- subset(data, region==rgn & aanum >= start & aanum <= end)
-	data.subset$replicate <- factor(data.subset$replicate)
-	data.subset$aanum <- factor(data.subset$aanum)
+
+	data.subset <- getCodonCountSubset(config,subject,region,'aa', start, end)
+	
 	print(data.subset[which(data.subset$aanum==aanum),splitFields('replicate,rank,aa,count,freq')])
 	#print(head(data.subset))
 	numcol <- length(unique(data.subset$rank))
@@ -623,15 +630,14 @@ reportAminoAcidChange <- function(config, subject, region, log=TRUE, updown=5)
 }
 #reportAminoAcidChange(config,'PXB0218-0007','NS3aa156')
 
-
 reportAminoAcidChanges <- function(config, subject=NULL, ...)
 {
 	if (is.null(subject))
 		subjects <- unique(config@samples$subject)
-	else subects <- c(subject)
+	else subjects <- c(subject)
 	for (subject in subjects)
 	{
-		filename <- concat(config@out.dir,subject,'.pdf')
+		filename <- concat(config@out.dir,subject,'.aa.pdf')
 		pdf(filename)
 		samples <- config@samples[which(config@samples$subject==subject),'sample']
 		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
@@ -644,3 +650,165 @@ reportAminoAcidChanges <- function(config, subject=NULL, ...)
 }
 #reportAminoAcidChanges(config, 'PXB0218-0007', log=FALSE)
 
+########################################################################
+
+reportCodonChange <- function(config, subject, region, log=FALSE, updown=5)
+{
+	aanum <- config@regions[region,'focus']
+	if (!is.integer(aanum))
+		stop(concat('cannot find focus aanum for region: ',region,' (',aanum,')'))
+	aanum <- as.integer(aanum)	
+	start <- aanum - updown
+	end <- aanum + updown
+	
+	data.subset <- getCodonCountSubset(config,subject,region,'codons', start, end)
+	
+	print(data.subset[which(data.subset$aanum==aanum),splitFields('replicate,rank,aa,codon,count,freq')])
+	#print(head(data.subset))
+	numcol <- length(unique(data.subset$rank))
+	col <- gray(numcol:0 / numcol)
+	if (log)
+		frmla <- as.formula(log10(count) ~ aanum | replicate)
+	else frmla <- as.formula(count ~ aanum | replicate)
+	chrt <- barchart(frmla, data.subset, group=rank,
+			horizontal=FALSE, stack=TRUE, main=subject, xlab='Codon number', sub=region,
+			col=col, strip=FALSE, strip.left=TRUE, #strip.text = list(cex = 0.75),
+			#auto.key = list(space = "right"),
+			layout = c(1,length(unique(data.subset$replicate))))
+	print(chrt)
+	addLine(v=aanum - start + 1 - 0.5, col='red', lty=2)
+	addLine(v=aanum - start + 1 + 0.5, col='red', lty=2)
+	return(data.subset)
+}
+#reportCodonChange(config,'PXB0218-0007','NS3aa156')
+
+reportCodonChanges <- function(config, subject=NULL, ...)
+{
+	if (is.null(subject))
+		subjects <- unique(config@samples$subject)
+	else subjects <- c(subject)
+	for (subject in subjects)
+	{
+		filename <- concat(config@out.dir,subject,'.codons.pdf')
+		pdf(filename)
+		samples <- config@samples[which(config@samples$subject==subject),'sample']
+		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
+		for (region in regions)
+		{
+			reportCodonChange(config, subject, region, ...)
+		}
+		dev.off()
+	}
+}
+#reportCodonChanges(config, 'PXB0218-0007')
+
+#######################################################################3
+
+makeCodonBarchart <- function(config, subject, region)
+{
+	aanum <- as.integer(config@regions[region,'focus'])
+	data.subset <- getCodonCountSubset(config,subject,region,'codons',aanum)
+	chrt <- barchart(count ~ replicate, data.subset, group=codon, horizontal=FALSE, stack=TRUE,
+			main=subject, sub=concat('codon ',aanum), xlab='Replicate', ylab='Count',
+			auto.key = list(space = "right"))
+	print(chrt)
+}
+#makeCodonBarchart(config,'PXB0218-0007','NS3aa156')
+
+makeAminoAcidBarchart <- function(config, subject, region)
+{
+	aanum <- as.integer(config@regions[region,'focus'])
+	data.subset <- getCodonCountSubset(config,subject,region,'aa',aanum)
+	chrt <- barchart(count ~ replicate, data.subset, group=aa, horizontal=FALSE, stack=TRUE,
+			main=subject, sub=concat('amino acid ',aanum), xlab='Replicate', ylab='Count',
+			auto.key = list(space = "right"))
+	print(chrt)
+}
+#makeAminoAcidBarchart(config,'PXB0218-0007','NS3aa156')
+
+makeAminoAcidBarcharts <- function(config, subject=NULL, ...)
+{
+	if (is.null(subject))
+		subjects <- unique(config@samples$subject)
+	else subjects <- c(subject)
+	for (subject in subjects)
+	{
+		filename <- concat(config@out.dir,subject,'.aa2.pdf')
+		pdf(filename)
+		samples <- config@samples[which(config@samples$subject==subject),'sample']
+		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
+		for (region in regions)
+		{
+			makeAminoAcidBarchart(config, subject, region, ...)
+		}
+		dev.off()
+	}
+}
+#makeAminoAcidBarcharts(config, 'PXB0218-0007')
+
+makeCodonBarcharts <- function(config, subject=NULL, ...)
+{
+	if (is.null(subject))
+		subjects <- unique(config@samples$subject)
+	else subjects <- c(subject)
+	for (subject in subjects)
+	{
+		filename <- concat(config@out.dir,subject,'.codons2.pdf')
+		pdf(filename)
+		samples <- config@samples[which(config@samples$subject==subject),'sample']
+		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
+		for (region in regions)
+		{
+			makeCodonBarchart(config, subject, region, ...)
+		}
+		dev.off()
+	}
+}
+#makeCodonBarcharts(config, 'PXB0218-0007')
+
+#############################################################3
+
+
+
+makeAminoAcidTables <- function(config, subject=NULL, ...)
+{
+	if (is.null(subject))
+		subjects <- unique(config@samples$subject)
+	else subjects <- c(subject)
+	for (subject in subjects)
+	{
+		filename <- concat(config@out.dir,subject,'.aa.txt')
+		samples <- config@samples[which(config@samples$subject==subject),'sample']
+		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
+		for (region in regions)
+		{
+			tbl <- makeAminoAcidTable(config, subject, region, ...)
+			print(concat('subject: ',subject))
+			print(concat('region: ',region))
+			print(tbl)
+		}
+	}
+}
+#makeAminoAcidTables(config, 'PXB0218-0007')
+
+
+makeCodonTables <- function(config, subject=NULL, ...)
+{
+	if (is.null(subject))
+		subjects <- unique(config@samples$subject)
+	else subjects <- c(subject)
+	for (subject in subjects)
+	{
+		filename <- concat(config@out.dir,subject,'.aa.txt')
+		samples <- config@samples[which(config@samples$subject==subject),'sample']
+		regions <- unique(config@runs[which(config@runs$sample %in% samples),'region'])
+		for (region in regions)
+		{
+			tbl <- makeCodonTable(config, subject, region, ...)
+			print(concat('subject: ',subject))
+			print(concat('region: ',region))
+			print(tbl)
+		}
+	}
+}
+#makeCodonTables(config, 'PXB0218-0007')
