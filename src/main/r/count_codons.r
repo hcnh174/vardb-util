d1edb990496e854af43f74c8d54422d9b8b377c5
hcnh174 <nelsonhayes4@gmail.com>
2011-07-30 00:40:29 +0900

    corrected argument processing in count_codons.r

--- src/main/r/count_codons.r
+++ src/main/r/count_codons.r
@@ -8,16 +8,17 @@ config <- new('nextgenconfig')
 #config@variants.dir <- 'c:/temp/variants-bwa/'
 #config@variants.dir <- 'f:/variants/'
 
-args <- commandArgs(TRUE)
-subject <- args[1]
+args <- commandArgs(TRUE) # from R.utils package
+subject <- args$subject
 
 #subject <- 'PXB0219-0011'
 #subject <- 'KT9'
 
-params <- new('sampleparams', subject=subject)
 print(concat('subject=',subject))
+params <- new('sampleparams', subject=subject)
 
-#Rscript c:/workspace/vardb-util/src/main/r/count_codons.r KT9
+
+#Rscript c:/workspace/vardb-util/src/main/r/count_codons.r subject=KT9
 #Rscript count_codons.r KT9
 #Rscript count_codons.r PXB0218-0007
 #Rscript count_codons.r PXB0219-0011
